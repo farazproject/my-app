06ae9a474812d550e84bc06dfb3744208a68f732
farazproject <muhammad.gulfaraz@live.com>
2025-05-20 00:44:58 +0500

    react work done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
+"use client"
 import Image from "next/image";
 import Header from "@/components/header/header";
-import { Geist, Geist_Mono } from "next/font/google";
+import Teams from "@/components/cards/teams";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+  const matches =[
+    {
+      team1 : "Liverpool",
+      team2 : "Manchester City",
+      league : "Premier League",
+      time : "20:15",
+      date : "Thu 07 Dec",
+      stadiunName : "artifield Stadium"
+    },
+        {
+      team1 : "chelsia",
+      team2 : "manchester united",
+      league : "Premier League",
+      time : "20:15",
+      date : "Thu 07 Dec",
+      stadiunName : "artifield Stadium"
+    },
+        {
+      team1 : "Arsenal",
+      team2 : "Liester city",
+      league : "Premier League",
+      time : "20:15",
+      date : "Thu 07 Dec",
+      stadiunName : "artifield Stadium"
+    }
+  ]
+
+
+
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Header/>
+      <Teams/>
 
     </div>
   );
